88dad45c7d68dcd32041b25424dcfa84731a69ef
michaawl <117541955+michaawl@users.noreply.github.com>
2024-11-04 10:51:17 +0100

    Init
    initial commit

---
+++ contdel-app/src/main/java/at/fhj/msd/App.java
@@ -0,0 +1,13 @@
+package at.fhj.msd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
